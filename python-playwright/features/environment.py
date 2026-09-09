769aa04cff74b9cc78adc7b0cc8fa106b7671897
Filip <wierzbickifilip94@gmail.com>
2026-09-09 16:07:12 +0200

    ci(behave): add smoke/regression tags, fix headless mode for CI

--- python-playwright/features/environment.py
+++ python-playwright/features/environment.py
@@ -1,9 +1,13 @@
+import os
+
 from playwright.sync_api import sync_playwright
+
 
 def before_all(context):
     context.base_url = "http://uitestingplayground.com"
     context.playwright = sync_playwright().start()
-    context.browser = context.playwright.chromium.launch(headless=False)
+    is_ci = os.environ.get("CI", "false").lower() == "true"
+    context.browser = context.playwright.chromium.launch(headless=is_ci)
 
 def before_scenario(context, scenario):
     context.page = context.browser.new_page()
