9c1f591dfe2b96e01bd9ca94cbd86d166d96f25d
Filip <wierzbickifilip94@gmail.com>
2026-09-18 12:58:59 +0200

    feat(tests): add non-breaking space visibility test

--- python-playwright/tests/test_01_click.py
+++ python-playwright/tests/test_01_click.py
@@ -1,11 +1,7 @@
-#import re
-from playwright.sync_api import Page
 import pytest
+from playwright.sync_api import Page
 
 @pytest.mark.regression
 def test_click_button(page: Page):
     page.goto("http://uitestingplayground.com/click")
     page.click("#badButton")
-    #expect(page.locator("#badButton")).to_have_class(re.compile("btn-success"))
-
-    #test
