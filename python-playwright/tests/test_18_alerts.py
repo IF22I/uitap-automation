26132ff019a5d80d535783e530b5ef768f5ab24a
Filip <wierzbickifilip94@gmail.com>
2026-09-22 14:16:37 +0200

    test(playground): add Exercise 18 (Alerts) in Python

--- python-playwright/tests/test_18_alerts.py
+++ python-playwright/tests/test_18_alerts.py
@@ -5,19 +5,20 @@ from playwright.sync_api import Page
 def test_alerts(page: Page):
     page.goto("http://uitestingplayground.com/alerts")
 
-    page.on("dialog", lambda dialog: dialog.accept())
+    last_message = None
+
+    def handle_dialog(dialog):
+        print(f"Dialog appeared: type={dialog.type}, message={dialog.message}")
+        nonlocal last_message
+        last_message = dialog.message
+        if dialog.type == "prompt":
+            dialog.accept("my answer")
+        else:
+            dialog.accept()
+
+    page.on("dialog", handle_dialog)
     page.click("#alertButton")
+    page.click("#confirmButton")
+    page.click("#promptButton")
 
-
-
-
-
-
-    #page.click("#confirmButton")
-
-
-
-
-
-
-    #page.click("#promptButton")
+    assert "User value: my answer" in last_message
